9b309463c13149ad61fb1e8d0e0d62de1c017f26
kateobi <akinyi180@gmail.com>
2020-10-31 22:37:02 +0300

    updated duka

--- soko/settings.py
+++ soko/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b6=mhuy+qntyxp5d!l$owa!sfn0by)9ki$2&mbfcnhpot$8pp$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 # Application definition
 
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/geopro/Desktop/Coding/soko/venv"
+set -gx VIRTUAL_ENV "/home/admin/PycharmProjects/duka/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
